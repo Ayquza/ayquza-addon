01ab1e46017add3edb5a1772fa32f399264e4746
ayquza <twitch.zslaitz@gmail.com>
2026-02-28 18:16:44 +0100

    Updated ClearCurrentServer Module

--- src/main/java/com/ayquza/addon/mixins/MultiplayerScreenAccessor.java
+++ src/main/java/com/ayquza/addon/mixins/MultiplayerScreenAccessor.java
@@ -1,13 +1,16 @@
-
 package com.ayquza.addon.mixins;
 
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.option.ServerList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(MultiplayerScreen.class)
 public interface MultiplayerScreenAccessor {
     @Accessor("serverList")
     ServerList getServerList();
+
+    @Invoker("removeEntry")
+    void invokeRemoveEntry(boolean confirmedAction);
 }
--- src/main/java/com/ayquza/addon/modules/ClearCurrentServer.java
+++ src/main/java/com/ayquza/addon/modules/ClearCurrentServer.java
@@ -13,17 +13,29 @@ import meteordevelopment.meteorclient.utils.misc.Keybind;
 
 public class ClearCurrentServer extends Module {
 
-    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    private final SettingGroup sgIngame = settings.createGroup("In-Game");
+    private final SettingGroup sgServerList = settings.createGroup("Server List");
 
-    private final Setting<Keybind> removeKey = sgGeneral.add(new KeybindSetting.Builder()
-        .name("remove-key")
-        .description("Taste um den aktuellen Server aus der Liste zu entfernen.")
+    private final Setting<Keybind> removeKey = sgIngame.add(new KeybindSetting.Builder()
+        .name("remove-ingame-server-key")
+        .description("Key to remove the server you are currently connected to from the Multiplayer Server list.")
+        .defaultValue(Keybind.fromKey(GLFW.GLFW_KEY_UNKNOWN))
+        .build()
+    );
+
+    private final Setting<Keybind> removeSelectedKey = sgServerList.add(new KeybindSetting.Builder()
+        .name("remove-selected-server-key")
+        .description("Key to remove the selected server in the multiplayer menu.")
         .defaultValue(Keybind.fromKey(GLFW.GLFW_KEY_UNKNOWN))
         .build()
     );
 
     public ClearCurrentServer() {
-        super(AyquzaAddon.CATEGORY, "clear-current-server", "Entfernt den aktuellen Server aus der Multipayer-Liste.");
+        super(AyquzaAddon.CATEGORY, "clear-current-server", "Removes servers from the multiplayer list via keybinds.");
+    }
+
+    public Keybind getRemoveSelectedKey() {
+        return removeSelectedKey.get();
     }
 
     @EventHandler
@@ -31,19 +43,18 @@ public class ClearCurrentServer extends Module {
         if (!removeKey.get().isPressed()) return;
 
         MinecraftClient mc = MinecraftClient.getInstance();
-        if (mc.getCurrentServerEntry() == null) return; // Nicht auf einem Server
+        if (mc.getCurrentServerEntry() == null) return;
 
         ServerInfo current = mc.getCurrentServerEntry();
         ServerList serverList = new ServerList(mc);
-        serverList.loadFile(); // Liste laden
-
+        serverList.loadFile();
 
         for (int i = 0; i < serverList.size(); i++) {
             ServerInfo info = serverList.get(i);
             if (info.address.equalsIgnoreCase(current.address)) {
                 serverList.remove(info);
-                serverList.saveFile(); // Speichern
-                info("Server §c" + info.name + "§r wurde aus der Liste entfernt.");
+                serverList.saveFile();
+                info("Server §c" + info.name + "§r removed from list.");
                 break;
             }
         }
